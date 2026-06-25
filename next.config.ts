e8270e315a822be878070f6a45c12e8334ec7fbb
Nguyen Tuan Hiep <hiep454546@gmail.com>
2026-06-26 01:15:22 +0800

    fix favicon

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1600, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 86400,
+    qualities: [88],
     remotePatterns: [
       {
         protocol: "https",
